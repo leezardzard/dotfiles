ed1fbf037eb6c9aa17ba27f328f01ef18117abc4
Dominic Lee <dominiclee0512@gmail.com>
2022-06-07 10:14:27 +0800

    feat: add fish bootstrap script.

---
+++ scripts/bootstrap.fish
@@ -0,0 +1,20 @@
+#!/path/to/fish
+
+###############################################################################
+# Install fish prompt theme
+###############################################################################
+brew tap homebrew/cask-fonts
+brew install --cask font-hack-nerd-font
+fisher install IlanCosman/tide@v5
+
+###############################################################################
+# Install packages
+###############################################################################
+fisher install jorgebucaran/nvm.fish
+fisher install jethrokuan/z
+
+###############################################################################
+# Install brew packages for fish settings
+###############################################################################
+brew install exa
+brew install peco
