5bdf75e031abdeb59aec7c202f639de9ee452e9d
Dominic <dominiclee0512@gmail.com>
2022-07-22 09:52:20 +0800

    fix: resolve the fuck command not working on fish shell issue.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -55,5 +55,13 @@ if test -f $LOCAL_CONFIG
 end
 
 # The fuck 
-# Ref: https://github.com/nvbn/thefuck
-thefuck --alias | source
+# Ref: https://github.com/nvbn/thefuck/issues/730
+function fuck -d "Correct your previous console command"
+  set -l fucked_up_command $history[1]
+  env TF_ALIAS=fuck PYTHONIOENCODING=utf-8 thefuck $fucked_up_command | read -l unfucked_command
+  if [ "$unfucked_command" != "" ]
+    eval $unfucked_command
+    builtin history delete --exact --case-sensitive -- $fucked_up_command
+    builtin history merge ^ /dev/null
+  end
+end
