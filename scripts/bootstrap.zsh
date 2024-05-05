0428743b8395737e23710ffcb17ae299289a199d
Dominic <12182264+Dominic0512@users.noreply.github.co>
2024-05-05 22:27:14 +0800

    feat: add fzf theme setup

--- scripts/bootstrap.zsh
+++ scripts/bootstrap.zsh
@@ -88,6 +88,19 @@ cat <<'EOT' >>~/.zshrc
 # https://github.com/junegunn/fzf
 ###############################################################################
 eval "$(fzf --zsh)"
+
+###############################################################################
+# fzf theme setup
+# https://vitormv.github.io/fzf-themes/
+###############################################################################
+fg="#CBE0F0"
+bg="#011628"
+bg_highlight="#143652"
+purple="#B388FF"
+blue="#06BCE4"
+cyan="#2CF9ED"
+
+export FZF_DEFAULT_OPTS="--color=fg:${fg},bg:${bg},hl:${purple},fg+:${fg},bg+:${bg_highlight},hl+:${purple},info:${blue},prompt:${cyan},pointer:${cyan},marker:${cyan},spinner:${cyan},header:${cyan}"
 EOT
 
 ###############################################################################
