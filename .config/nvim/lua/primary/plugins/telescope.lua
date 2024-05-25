81ba3edaf07cffe696e52219aee5e7ee01b90fa8
Dominic <12182264+Dominic0512@users.noreply.github.co>
2024-05-25 13:45:21 +0800

    feat: add more nvim settings

--- .config/nvim/lua/primary/plugins/telescope.lua
+++ .config/nvim/lua/primary/plugins/telescope.lua
@@ -12,15 +12,15 @@ return {
     local actions = require("telescope.actions")
     local transform_mod = require("telescope.actions.mt").transform_mod
 
-    -- local trouble = require("trouble")
-    -- local trouble_telescope = require("trouble.providers.telescope")
+    local trouble = require("trouble")
+    local trouble_telescope = require("trouble.providers.telescope")
 
     -- or create your custom action
-    -- local custom_actions = transform_mod({
-    --   open_trouble_qflist = function(prompt_bufnr)
-    --     trouble.toggle("quickfix")
-    --   end,
-    -- })
+    local custom_actions = transform_mod({
+      open_trouble_qflist = function()
+        trouble.toggle("quickfix")
+      end,
+    })
 
     telescope.setup({
       defaults = {
@@ -28,9 +28,9 @@ return {
         mappings = {
           i = {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-            -- ["<C-t>"] = trouble_telescope.smart_open_with_trouble,
+            ["<C-j>"] = actions.move_selection_next,     -- move to next result
+            ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
+            ["<c-t>"] = trouble_telescope.smart_open_with_trouble,
           },
         },
       },
@@ -38,7 +38,7 @@ return {
         find_files = {
           hidden = true
         }
-      }
+      },
     })
 
     telescope.load_extension("fzf")
