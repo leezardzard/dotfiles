0fff501bd3eb277fe1cf3cf1141304d33d6fe41a
Dominic Lee <12182264+Dominic0512@users.noreply.github.com>
2025-04-22 10:21:04 +0800

    fix: resolve plugin deprecated issues

--- .config/nvim/lua/primary/plugins/telescope.lua
+++ .config/nvim/lua/primary/plugins/telescope.lua
@@ -11,9 +11,7 @@ return {
     local telescope = require("telescope")
     local actions = require("telescope.actions")
     local transform_mod = require("telescope.actions.mt").transform_mod
-
-    local trouble = require("trouble")
-    local trouble_telescope = require("trouble.providers.telescope")
+    local open_with_trouble = require("trouble.sources.telescope").open
 
     -- or create your custom action
     local custom_actions = transform_mod({
@@ -30,7 +28,7 @@ return {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next,     -- move to next result
             ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-            ["<c-t>"] = trouble_telescope.smart_open_with_trouble,
+            ["<c-t>"] = open_with_trouble,
           },
         },
       },
