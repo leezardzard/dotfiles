f291c8f1dc979e8c97e6f0d7fd042a7c69c36240
Dominic <12182264+Dominic0512@users.noreply.github.com>
2025-06-08 11:08:35 +0800

    fix: prevent compress video height can not divide properly

--- scripts/bootstrap-zshrc.zsh
+++ scripts/bootstrap-zshrc.zsh
@@ -302,6 +302,6 @@ compress_video() {
     local source_file=$1
     local destination_file=$2
 
-    ffmpeg -i "$source_file" -vcodec libx264 -preset fast -crf 20 -y -vf "scale=1920:-1" -acodec libmp3lame -ab 128k "$destination_file"
+    ffmpeg -i "$source_file" -vcodec libx264 -preset fast -crf 20 -y -vf "scale=1920:trunc(ow/a/2)*2" -acodec libmp3lame -ab 128k "$destination_file"
 }
 EOT
